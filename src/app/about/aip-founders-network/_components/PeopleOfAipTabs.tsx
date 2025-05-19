25a1bf503aa8168efc6353eaad78549d0621ceaa
afl-h <58439230+afl-h@users.noreply.github.com>
2025-05-19 13:03:18 +0530

    Update PeopleOfAipTabs.tsx

--- src/app/about/aip-founders-network/_components/PeopleOfAipTabs.tsx
+++ src/app/about/aip-founders-network/_components/PeopleOfAipTabs.tsx
@@ -18,42 +18,40 @@ const PeopleOfAipTabs = ({
   setOpenTab,
 }: Props) => {
   return (
-    <>
-      <div
-        className={cn(
-          "z-[200] w-full flex justify-center items-center ~pt-8/[4.75rem] ",
-          mainClassName
-        )}
-      >
-        <div className="flex w-full justify-center">
-          <div
-            className={cn(
-              "z-[200] flex bg-[#F0F0F2] p-1 rounded-full w-fit",
-              tabListClassName
-            )}
-          >
-            {tabList.map((items, i) => (
-              <div
-                onClick={() => {
-                  setOpenTab(items.slug);
-                }}
-                key={i}
-                className={cn(
-                  "~px-[1.5rem]/[2.5rem] py-[0.75rem] flex justify-center items-center ~text-h10Copy6/h9Copy5 ~leading-[1rem]/[1.22rem] font-inter ",
-                  tabClassName,
-                  {
-                    "!bg-darkPurple !text-white !outline-none !rounded-full":
-                      items.slug === currentTab,
-                  }
-                )}
-              >
-                {items.name}
-              </div>
-            ))}
-          </div>
+    <div
+      className={cn(
+        "z-[200] w-full flex justify-center items-center ~pt-8/[4.75rem] ",
+        mainClassName
+      )}
+    >
+      <div className="flex w-full justify-center">
+        <div
+          className={cn(
+            "z-[200] flex bg-[#F0F0F2] p-1 rounded-full w-fit",
+            tabListClassName
+          )}
+        >
+          {tabList.map((items, i) => (
+            <button
+              onClick={() => {
+                setOpenTab(items.slug);
+              }}
+              key={i}
+              className={cn(
+                "~px-[1.5rem]/[2.5rem] py-[0.75rem] flex justify-center items-center ~text-h10Copy6/h9Copy5 ~leading-[1rem]/[1.22rem] font-inter cursor-pointer ",
+                tabClassName,
+                {
+                  "!bg-darkPurple !text-white !outline-none !rounded-full":
+                    items.slug === currentTab,
+                }
+              )}
+            >
+              {items.name}
+            </button>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
